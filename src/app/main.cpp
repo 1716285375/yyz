1b3db10dfce5821db34fa36c3cf4188d19fb0fe1
1716285375 <1716285375@qq.com>
2025-08-02 02:41:06 +0800

    init project

---
+++ src/app/main.cpp
@@ -0,0 +1,13 @@
+
+
+#include <QApplication>
+#include <QSize>
+#include <login.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Login w;
+    w.show();
+    return a.exec();
+}
